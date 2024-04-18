27ec8dd292d2689f8b982a3913d51e048c08bb71
PRATHAMPARLECHA <prathamparlecha09@gmail.com>
2024-04-18 23:58:42 +0530

    final changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.VITE_apiKey": JSON.stringify(env.VITE_apiKey),
+      "process.env.VITE_authDomain": JSON.stringify(env.VITE_authDomain),
+      "process.env.VITE_projectId": JSON.stringify(env.VITE_projectId),
+      "process.env.VITE_storageBucket": JSON.stringify(env.VITE_storageBucket),
+      "process.env.VITE_messagingSenderId": JSON.stringify(
+        env.VITE_messagingSenderId
+      ),
+      "process.env.VITE_appId": JSON.stringify(env.VITE_appId),
+    },
+    plugins: [react()],
+  };
+});
